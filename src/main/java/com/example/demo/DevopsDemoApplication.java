9f39ab4d3e86746ece71ad72a0703e32c1d03f9c
sungjun1116 <opr99@naver.com>
2023-05-31 14:13:07 +0900

    packaging jar > war

--- src/main/java/com/example/demo/DevopsDemoApplication.java
+++ src/main/java/com/example/demo/DevopsDemoApplication.java
@@ -2,9 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DevopsDemoApplication {
+public class DevopsDemoApplication extends SpringBootServletInitializer {
+
+	// 이 부분 추가
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DevopsDemoApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsDemoApplication.class, args);
